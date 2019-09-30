19812f24930f153ff5d3a5875f8831d626c91a91
Unknown <eliocampitelli@gmail.com>
2019-09-30 12:06:57 -0300

    Commit

---
+++ R/dummy.R
@@ -0,0 +1,44 @@
+#' A function that returns TRUE
+#'
+#' This function does nothing. It merely returns `TRUE`.
+#'
+#' @param x first argument. Ignored.
+#' @param one_argument second argument. Also ignored.
+#' @param another_argument third arument. You guessed it: Ignored.
+#'
+#' @details
+#' Nothing to see here, move along.
+#'
+#' @examples
+#' # This example shows how useless this function is
+#' dummy_function()
+#'
+#' @language en
+#' @export
+dummy_function <- function(x, one_argument, another_argument) {
+  return(TRUE)
+}
+
+
+#' Una función que devuelve `TRUE``
+#'
+#' Esta función no hace nada. Sólo deuvelve `TRUE`.
+#'
+#' @param x primer argumento. Ignorado.
+#' @param one_argument segundo argumento. También ignorado.
+#' @param another_argument tercer argumento. Adivinaste: ignorado.
+#'
+#' @details
+#' Nada para ver acá.
+#'
+#' @examples
+#' # Este es un ejemplo para mostrar lo inutil que es la función.
+#' dummy_function()
+#'
+#' @language es
+#' @rdname dummy_function
+NULL
+
+
+
+
