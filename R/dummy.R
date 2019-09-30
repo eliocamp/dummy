0c7d9fd380fab1b95ab2a37c90cf34acd376c104
Unknown <eliocampitelli@gmail.com>
2019-09-30 12:24:00 -0300

    Splits up file

--- R/dummy.R
+++ R/dummy.R
@@ -19,26 +19,3 @@ dummy_function <- function(x, one_argument, another_argument) {
   return(TRUE)
 }
 
-
-#' Una función que devuelve `TRUE``
-#'
-#' Esta función no hace nada. Sólo deuvelve `TRUE`.
-#'
-#' @param x primer argumento. Ignorado.
-#' @param one_argument segundo argumento. También ignorado.
-#' @param another_argument tercer argumento. Adivinaste: ignorado.
-#'
-#' @details
-#' Nada para ver acá.
-#'
-#' @examples
-#' # Este es un ejemplo para mostrar lo inutil que es la función.
-#' dummy_function()
-#'
-#' @language es
-#' @rdname dummy_function
-NULL
-
-
-
-
---
+++ R/dummy_es.R
@@ -0,0 +1,18 @@
+#' Una función que devuelve `TRUE``
+#'
+#' Esta función no hace nada. Sólo deuvelve `TRUE`.
+#'
+#' @param x primer argumento. Ignorado.
+#' @param one_argument segundo argumento. También ignorado.
+#' @param another_argument tercer argumento. Adivinaste: ignorado.
+#'
+#' @details
+#' Nada para ver acá.
+#'
+#' @examples
+#' # Este es un ejemplo para mostrar lo inutil que es la función.
+#' dummy_function()
+#'
+#' @language es
+#' @rdname dummy_function
+NULL
