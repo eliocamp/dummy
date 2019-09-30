06a140154b6492ac32745cf8943a8dd62c7c0f89
Unknown <eliocampitelli@gmail.com>
2019-09-30 14:25:54 -0300

    Adds more examples

--- R/dummy.R
+++ R/dummy.R
@@ -13,9 +13,53 @@
 #' # This example shows how useless this function is
 #' dummy_function()
 #'
-#' @language en
+#' @language en 1.0.0
 #' @export
 dummy_function <- function(x, one_argument, another_argument) {
   return(TRUE)
 }
 
+
+#' Another useless function
+#'
+#' This function returns `FALSE`.
+#'
+#' @param x it's the only parameter and it's ignored.
+#'
+#' @details
+#' Why would you use this function instead of just typing `FALSE`?
+#'
+#' @examples
+#' # Proof that it works
+#' FALSE == dummy_function2()
+#'
+#' @language en 1.0.1
+#' @export
+dummy_function2 <- function(x) {
+  return(FALSE)
+}
+
+
+#' Do nothing
+#'
+#' This function is so lazy it does nothing at all.
+#'
+#' @param x useless parameter
+#'
+#' @language em 1.1.0
+#' @export
+dummy_function3 <- function(x) {
+}
+
+
+#' Add 2
+#'
+#' This function used to add 1, but now it adds 2.
+#'
+#' @param x numeric
+#'
+#' @language em 2.0.0
+#' @export
+dummy_function4 <- function(x) {
+  x + 2
+}
--- R/dummy_es.R
+++ R/dummy_es.R
@@ -1,3 +1,5 @@
+## Up-to-date translation
+#  Mirrors the last version of the original documentation.
 #' Una función que devuelve `TRUE``
 #'
 #' Esta función no hace nada. Sólo deuvelve `TRUE`.
@@ -13,6 +15,42 @@
 #' # Este es un ejemplo para mostrar lo inutil que es la función.
 #' dummy_function()
 #'
-#' @language es
+#' @language es 1.0.0
 #' @rdname dummy_function
+NULL
+
+
+## Missing translation
+#  This documentation entry needs translation.
+#  (Should it even be here, probably not)
+#' @language en 0.0.0
+#' @rdname dummy_function2
+NULL
+
+
+## Slightly outdated translation
+#  It's still perfectly usable, but not exactly up to date.
+#' No hace nada
+#'
+#' Esta función no hace nada.
+#'
+#' @param x parametro ignorado
+#'
+#'
+#' @language es 1.0.0
+#' @rdname dummy_function3
+NULL
+
+
+## Wholy outdated translation.
+#  Users will not get a good idea of what the function does or how it works
+#  anymore.
+#' Suma 1
+#'
+#' Esta función suma 1.
+#'
+#' @param x numeric
+#'
+#' @language es 1.0.0
+#' @rdname dummy_function4
 NULL
